6e35c1a869df05715c5f8fb2150834397908c4ab
elseptiawan <septiawanelshandi@gmail.com>
2022-10-23 15:18:32 +0700

    Create Model User

---
+++ models/User.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, Datatypes) => {
+    const User = sequelize.define['User', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          position: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          role: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          foto_profil: {
+            type: DataTypes.STRING
+          },
+          createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+          updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+          },
+    }, {
+        tableName : 'users'
+    }];
+
+    return User;
+}
