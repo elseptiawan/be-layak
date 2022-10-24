6a8de6e18e40c0e8bef98f4e0df43dbdf27c0195
elseptiawan <septiawanelshandi@gmail.com>
2022-10-24 23:55:23 +0700

    Edit User Model

--- models/User.js
+++ models/User.js
@@ -12,9 +12,14 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false
           },
-          email: {
+          nama: {
             type: DataTypes.STRING,
             allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
           },
           password: {
             type: DataTypes.STRING,
