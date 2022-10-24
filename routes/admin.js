bbc3124e5770edacaa04b85894978c14bbdef7e2
elseptiawan <septiawanelshandi@gmail.com>
2022-10-24 23:16:49 +0700

    API Create User Sementara

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const Validator = require('fastest-validator');
+var bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
 
-// const { User } = require('../models');
+const { User, Company } = require('../models');
 
 const v = new Validator();
 
 
 router.post('/users', async (req, res) => {
     const schema = {
+        nama: 'string',
         email: 'string',
         password: 'string',
         position: 'string',
-        foto_profil: 'string',
-        company_id: 'number'
+        company_id: 'number',
     }
 
     const validate = v.validate(req.body, schema);
@@ -22,7 +24,22 @@ router.post('/users', async (req, res) => {
         return res.status(400).json(validate);
     }
 
-    
+    const company = await Company.findOne({
+        where: {
+            id: req.body.company_id
+        }
+    });
+
+    const user = await User.create({
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, salt),
+        position: req.body.position,
+        role: "User",
+        sisa_cuti: company.jatah_cuti,
+        company_id: req.body.company_id
+    });
+
+    res.json({success: "true", messages: "The account has been created", data: user});
   });
 
 module.exports = router;
