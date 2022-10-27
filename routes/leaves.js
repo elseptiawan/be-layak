b9d34d0ee9da0be60aa223e311ec29285cd9dbd5
elseptiawan <septiawanelshandi@gmail.com>
2022-10-27 22:15:04 +0700

    API Create Pengajuan Cuti

--- routes/leaves.js
+++ routes/leaves.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const Validator = require('fastest-validator');
 
 const { Leave } = require('../models');
+const v = new Validator();
 
 router.get('/', async (req, res) => {
     const leave = await Leave.findAll({
@@ -18,6 +20,37 @@ router.get('/:id', async (req, res) => {
     });
 
     res.json({success: "true", messages: "Data retrieved successfully", data: leave || {}}); 
+});
+
+router.post('/', async (req, res) => {
+    const schema = {
+        tipe_cuti: 'string',
+        start_date: {
+            type: "date",
+            convert: true
+        },
+        end_date: {
+            type: "date",
+            convert: true
+        },
+        surat_cuti: 'string'
+    }
+
+    const validate = v.validate(req.body, schema);
+
+    if(validate.length){
+        return res.status(400).json(validate);
+    }
+
+    const leave = await Leave.create({
+        user_id: req.body.user_id,
+        tipe_cuti: req.body.tipe_cuti,
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        surat_cuti: req.body.surat_cuti
+    });
+
+    res.json({success: "true", messages: "Your application for leave is success", data: leave}); 
 });
 
 
