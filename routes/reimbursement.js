e7742636eed1c55e5a3b2af62ecfb9b799df52e6
elseptiawan <septiawanelshandi@gmail.com>
2022-10-28 07:26:19 +0700

    API Create Reimbursement

--- routes/reimbursement.js
+++ routes/reimbursement.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const Validator = require('fastest-validator');
+const multer = require('multer');
 
 const { Reimbursement } = require('../models');
+const v = new Validator();
+
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'Storages/Reimbursements');
+    },
+    filename: function(req, file, cb) {
+        const originalName = file.originalname;
+        const nameArr = originalName.split('.');
+        var extension = '';
+        if (nameArr.length > 1){
+            extension = nameArr[nameArr.length - 1];
+        }
+
+        cb(null, file.fieldname +'-'+ Date.now() +'-'+ extension);
+    }
+});
+
+const multerUpload = multer({storage: multerDiskStorage});
 
 router.get('/', async (req, res) => {
     const reimbursement = await Reimbursement.findAll({
@@ -18,6 +39,38 @@ router.get('/:id', async (req, res) => {
     });
 
     res.json({success: "true", messages: "Data retrieved successfully", data: reimbursement || {}});
+});
+
+router.post('/', multerUpload.single('bukti_pembayaran'), async (req, res) => {
+    const schema = {
+        jumlah_uang: 'string',
+        tanggal_pembayaran: {
+            type: "date",
+            convert: true
+        },
+        kebutuhan: 'string'
+    }
+
+    const validate = v.validate(req.body, schema);
+
+    if(validate.length){
+        return res.status(400).json(validate);
+    }
+
+    const bukti_pembayaran = req.file;
+    if(!bukti_pembayaran){
+        return res.status(400).json({success: "false", messages: "Surat Cuti cannot be empty"});
+    }
+
+    const reimbursement = await Reimbursement.create({
+        user_id: req.body.user_id,
+        jumlah_uang: parseInt(req.body.jumlah_uang),
+        tanggal_pembayaran: req.body.tanggal_pembayaran,
+        bukti_pembayaran: bukti_pembayaran.filename,
+        kebutuhan: req.body.kebutuhan
+    });
+
+    res.json({success: "true", messages: "Your application for reimbursement is success", data: reimbursement}); 
 });
 
 module.exports = router;
