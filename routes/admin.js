a96d77eb460c4243578cb7d2d81c45feafaef74d
elseptiawan <septiawanelshandi@gmail.com>
2022-11-01 10:40:20 +0700

    edit api admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,106 @@ var router = express.Router();
 const Validator = require('fastest-validator');
 var bcrypt = require('bcrypt');
 var salt = bcrypt.genSaltSync(10);
+const { verifyToken, checkUser } = require('../middleware/authJWT.js');
 
-const { User, Company } = require('../models');
+const { User, Company, Presence } = require('../models');
 
 const v = new Validator();
+const { Op } = require("sequelize");
 
+router.get('*', checkUser);
+router.post('*', checkUser);
+router.get('/presences', verifyToken, async (req, res) => {
+    var nowDate = new Date(); 
+    var date = nowDate.getFullYear()+'-'+(nowDate.getMonth()+1)+'-'+nowDate.getDate();
+    const admin = await User.findByPk(req.id);
+    const presences = await Presence.findAll({
+        where: {
+            createdAt: {
+                [Op.lt]: new Date(),
+                [Op.gt]: date
+              }
+        },
+        include: {
+            model: User,
+            as: 'user',
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                company_id: admin.company_id
+            }
+        }
+    })
 
-router.post('/users', async (req, res) => {
+    res.json({success: "true", messages: "Data retrieved successfully", data: presences})
+});
+
+router.get('/presences/yet', verifyToken, async (req, res) => {
+    return res.send('KALEM BOS BELUM JALAN')
+    var nowDate = new Date(); 
+    var date = nowDate.getFullYear()+'-'+(nowDate.getMonth()+1)+'-'+nowDate.getDate();
+    const admin = await User.findByPk(req.id);
+    const user = await User.findAll({
+        where: {
+            company_id: admin.company_id,
+            role: 'User'
+        },
+        attributes: {
+            exclude: ['password']
+        }
+    });
+    const user_id = await Presence.findAll({
+        attributes: ['user_id'],
+        where: {
+            createdAt: {
+                [Op.lt]: new Date(),
+                [Op.gt]: date
+            }
+        }
+    });
+    user.forEach(async function(entry) {
+        user_id.forEach(async function(en){
+            if(en == entry.id){
+                return;
+            }
+            const test = await Presence.create({
+                user_id: entry.id
+            });
+            return res.json(test);
+        });
+        if (!user_id.includes(entry.id)){
+            return res.send('OK');
+            // await Presence.create({
+            //     user_id: entry.id
+            // });
+        }
+    });
+    return res.send('KO');
+    const presences = await Presence.findAll({
+        where: {
+            createdAt: {
+                [Op.lt]: new Date(),
+                [Op.gt]: date
+            },
+            clock_in: null
+        },
+        include: {
+            model: User,
+            as: 'user',
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                company_id: admin.company_id
+            }
+        }
+    })
+
+    res.json({success: "true", messages: "Data retrieved successfully", data: presences})
+});
+
+router.post('/users', verifyToken, async (req, res) => {
     const schema = {
         nama: 'string',
         email: 'string',
