958e589fed70cc4f458ccc3f8cb61bd658d14008
elseptiawan <septiawanelshandi@gmail.com>
2022-11-05 17:15:21 +0700

    fixing authentikasi

--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -1,22 +1,6 @@
 const jwt = require("jsonwebtoken");
 const {User} = require("../models");
 
-// const verifyToken = (req, res, next) => {
-//     let token = req.headers["x-access-token"];
-    
-//     if (!token) {
-//         return res.status(403).send({ message: "No token provided!" });
-//     }
-    
-//     jwt.verify(token, process.env.SECRET, (err, decoded) => {
-//         if (err) {
-//         return res.status(401).send({ message: "Unauthorized!" });
-//         }
-//         req.userId = decoded.id;
-//         next();
-//     });
-// }
-
 exports.verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
