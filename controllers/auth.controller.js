b122e5c450a5eb49a606db16769f5c0a90bcb165
elseptiawan <septiawanelshandi@gmail.com>
2022-11-08 08:02:30 +0700

    Merge Authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,38 +2,6 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcrypt');
 var { User } = require('../models');
 
-// POST /auth/login
-// exports.login = async(req, res) => {
-//     const user = await User.findOne({where: {email: req.body.email} }, function(err, user) {
-//         if (err) {
-//             return res.status(500).json({
-//                 title: 'An error occurred',
-//                 error: err
-//             });
-//         }
-//         if (!user) {
-//             return res.status(404).json({
-//                 title: 'Login failed',
-//                 error: { message: 'User not found' }
-//             });
-//         }
-//         // compare password, if not correct
-//         if (!bcrypt.compareSync(req.body.password, user.password)) {
-//             return res.status(401).json({
-//                 title: 'Login failed',
-//                 error: { message: 'Invalid login credentials' }
-//             });
-//         }
-//         // create token
-//         var token = jwt.sign({ user: user }, process.env.API_SECRET, { expiresIn: 86400 });
-//         res.status(200).json({
-//             message: 'Successfully logged in',
-//             token: token,
-//             userId: user._id
-//         });
-//     });
-// }
-
 exports.login = async(req, res) => {
     try {
         const user = await User.findOne({
@@ -54,5 +22,105 @@ exports.login = async(req, res) => {
         res.json({success: 'true', token: token});
     } catch (error) {
         res.status(400).json({success: 'false', message: 'wrong email'});
+    }
+}
+
+exports.logout = async(req, res) => {
+    try {
+        const userSchema = await User.findOne({
+            where:{
+                email: req.body.email
+            }
+        });
+        userSchema.token = null;
+        await userSchema.save();
+        res.status(200).json({
+            success: 'true', 
+            message: 'Logout successful',
+            data: {}
+        });
+    } catch (error) {
+        res.status(400).json({success: 'false', message: 'logout failed'});
+    }
+}
+
+exports.forgotPassword = async(req, res) => {
+    try {
+        const user = await User.findOne({
+            where:{
+                email: req.body.email
+            }
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+                status: 'error'
+            });
+        }
+
+        const resetToken = user.getResetPasswordToken();
+        await user.save({validateBeforeSave: false});
+        const resetUrl = `${req.protocol}://${req.get('host')}/api/auth/reset/${resetToken}`;
+        const message = `You are receiving this email because you have requested the reset of a password. Please access the following link to reset your password: \n\n ${resetUrl} \n\n If you did not request this, please ignore this email and your password will remain unchanged.`;
+        try {
+            await sendEmail({
+                email: user.email,
+                subject: 'Password reset token',
+                message
+            });
+            res.status(200).json({
+                success: 'true',
+                message: 'Reset token has been sent to your email',
+                data: {}
+            });
+        } catch(error) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+            await user.save({validateBeforeSave: false});
+            return next(new ErrorResponse('Email could not be sent', 500));
+        }
+    } catch(error) {
+        res.status(400).json({success: 'false', message: 'Email not found'});
+    }
+}
+
+exports.resetPassword = async(req, res) => {
+    try {
+        const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+        const user = await User.findOne({
+            where:{
+                email: req.body.email,
+                resetPasswordToken: resetPasswordToken,
+                resetPasswordExpire: {
+                    [Op.gt]: Date.now()
+                }
+            }
+        });
+
+        if(!user) {
+            return res.status(400).json({
+                success: 'false', 
+                message: 'Invalid Token',
+                data: {}
+            });
+        }
+
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save();
+
+        res.status(200).json({
+            success: 'true',
+            message: 'Password reset successful',
+            data: {}
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: 'false', 
+            message: 'Reset password failed'
+        });
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router(), 
 {
-login
+login,
+logout,
+forgotPassword,
+resetPassword,
 } = require('../controllers/auth.controller');
 const { verifyToken } = require('../middleware/authJWT.js');
 
 /* Post Login */
 router.post('/login', login);
+
+/* Post Logout */
+router.post('/logout', verifyToken, logout);
+
+/* Post Forgot Password */
+router.post('/forgot-password', forgotPassword);
+
+/* Post Reset Password */
+router.post('/reset-password', resetPassword);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
